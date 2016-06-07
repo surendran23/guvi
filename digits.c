15f77474df1b450cf9a627b3bbb3b774b106c121
surendran23 <vsurendran23@gmail.com>
2016-06-07 11:13:41 +0530

    Create digits.c

---
+++ digits.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int num,r=0,reverse;
+  printf("enter a number");
+  scanf("%d",&num);
+  while(num)
+  {
+     r=num%10;
+     reverse=reverse*10+r;
+     num=num/10;
+  }
+  printf("reversed number is %d",reverse); 
+  return 0;
+}  
