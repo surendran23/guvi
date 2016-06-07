7251e5398a30099e95af3c00892db37c4e731044
surendran23 <vsurendran23@gmail.com>
2016-06-07 11:26:54 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+ int main() {
+   int num;
+   char marks[3];
+   printf("Please Enter Marks : ");
+   scanf("%s", marks);
+   num = atoi(marks);
+   printf("\nMarks : %d", num);
+   return (0);
+}
